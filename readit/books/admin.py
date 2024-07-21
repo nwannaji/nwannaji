311c94251df34c525bc8dd1317e2fe353b4794e6
Henrio <edenwannaji1980@gmail.com>
2024-07-21 18:04:53 +0100

    Add .gitignore and modified the models.py and admin.py so that author names can appear under the authors column

--- readit/books/admin.py
+++ readit/books/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Books,Authors
+from .models import Book, Author
 
-# Register your models here.
-admin.site.register(Books)
-admin.site.register(Authors)
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Book Details", {"fields": ["title", "author"]}),
+        ("Review", {"fields": ["is_favorite", "review", "review_date"]}),
+    ]
+    readonly_fields = ("review_date",)
+    list_display = ("title", "display_authors", "review_date", "is_favorite")
 
+    def display_authors(self, obj):
+        return obj.list_authors()
+
+    display_authors.short_description = "Book Authors"
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
--- readit/books/models.py
+++ readit/books/models.py
@@ -1,25 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
-class Books(models.Model):
-    title = models.CharField(max_length=150)
-    author = models.ManyToManyField("Authors", related_name="books")
-    review = models.TextField(blank=True, null=True)
-    review_date = models.DateTimeField(blank=True, null=True)
-    is_favorite = models.BooleanField(default=False, verbose_name="Favourite?")
-
-    def __str__(self) -> str:
-        return "{} by {}".format(self.title, self.list_authors())
-    def list_authors(self):
-        return ", ".join([author.name for author in self.authors.all()])
-
-    
-class Authors(models.Model):
-    name = models.CharField(max_length=70, help_text="Use pen name, and not real name",
-                            unique= True)
+class Author(models.Model):
+    name = models.CharField(max_length=70, unique=True, help_text="Use pen name, not real name")
 
     def __str__(self):
         return self.name
 
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.ManyToManyField(Author, related_name="books")
+    review = models.TextField(blank=True, null=True)
+    review_date = models.DateTimeField(blank=True, null=True)
+    is_favorite = models.BooleanField(default=False, verbose_name="Favorite?")
 
+    def __str__(self):
+        return self.title
 
+    def list_authors(self):
+        return ", ".join([author.name for author in self.author.all()])
+
+    def save(self, *args, **kwargs):
+        if self.review and self.review_date is None:
+            self.review_date = timezone.now()  # Corrected assignment operator
+
+        super(Book, self).save(*args, **kwargs)
