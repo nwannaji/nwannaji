6255f8cfc261fe209f2fe3dc88b925820077f972
Henrio <edenwannaji1980@gmail.com>
2024-07-21 13:58:06 +0100

    Registered book in admin, and improve the books model

--- readit/books/models.py
+++ readit/books/models.py
@@ -3,8 +3,11 @@ from django.db import models
 # Create your models here.
 class Books (models.Model):
     title = models.CharField(max_length=150)
-    author = models.CharField(max_length=70)
+    author = models.CharField(max_length=70, help_text="Use pen name, not real name")
     review = models.TextField(blank=True, null=True)
     review_date = models.DateTimeField(blank=True, null=True)
-    is_favorite = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False, verbose_name="Favourite?")
+
+    def __str__(self) -> str:
+        return self.title
 
