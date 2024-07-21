71e7f83bdd70d2bf6556b06680fb088d5407679a
Henrio <edenwannaji1980@gmail.com>
2024-07-21 15:01:22 +0100

    Create many to many relations the author and the books and added new author

--- readit/books/admin.py
+++ readit/books/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Books
+from .models import Books,Authors
 
 # Register your models here.
 admin.site.register(Books)
+admin.site.register(Authors)
+
--- readit/books/models.py
+++ readit/books/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
 # Create your models here.
-class Books (models.Model):
+class Books(models.Model):
     title = models.CharField(max_length=150)
-    author = models.CharField(max_length=70, help_text="Use pen name, not real name")
+    author = models.ManyToManyField("Authors", related_name="books")
     review = models.TextField(blank=True, null=True)
     review_date = models.DateTimeField(blank=True, null=True)
     is_favorite = models.BooleanField(default=False, verbose_name="Favourite?")
 
     def __str__(self) -> str:
         return self.title
+    
+class Authors(models.Model):
+    name = models.CharField(max_length=70, help_text="Use pen name, and not real name",
+                            unique= True)
+
+    def __str__(self):
+        return self.name
+
+
 
