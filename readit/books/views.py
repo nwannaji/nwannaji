0611defc20637720f6feab36a998cce79a53fafc
Henrio <edenwannaji1980@gmail.com>
2024-07-22 15:00:37 +0100

    modified the template's list.html that user's time of logged in can be displayed if s/he is authenticated else, Welcome Guest! will be displayed

--- readit/books/views.py
+++ readit/books/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def list_books(request):
-    return HttpResponse(request.user.username)
+    return render(request, 'list.html')
