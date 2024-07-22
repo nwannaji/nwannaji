ba9f5016785e4cb1ad289b03bc516ed6c0ac2639
Henrio <edenwannaji1980@gmail.com>
2024-07-22 16:47:12 +0100

    I created diffrenr views, functional, class&functional to prove that is possible to use both, it all depend on individual preferences

--- readit/books/urls.py
+++ readit/books/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
-
+"""
+In the below URL decoration, I used both function base and class/function 
+decoration to show that one can either choose one  based on preference or use both.
+"""
 
 urlpatterns =[
- path('', views.list_books, name='books')
+ path('', views.list_books, name='books'),   # Function base url decoration
+ path('authors/', views.AuthorList.as_view(), name='authors'),  # Class & Function base url decoration
+ path('book-details/', views.BookDetails.as_view(), name='book-detals'),   # Class & Function base url decoration
+ path('author-details/', views.AuthorDetails.as_view(), name='book-detals'),   # Class & Function base url decoration
+
 ]
--- readit/books/views.py
+++ readit/books/views.py
@@ -1,5 +1,7 @@
+from django.db.models import Count
 from django.shortcuts import render
-from .models import Book
+from django.views import View
+from .models import Author, Book
 
 # Create your views here.
 
@@ -12,3 +14,33 @@ def list_books(request):
         'books':books
     }
     return render(request, 'list.html', context)
+class AuthorList(View):
+    def get(self, request):
+
+        authors = Author.objects.annotate(
+            published_books = Count('books')
+        ).filter(published_books__gt = 0)
+
+        context = {
+            'authors':authors
+        }
+        return render(request, 'authors.html',context)
+    
+class BookDetails(View):
+    def get(self, request):
+
+        bookdetails = Book.objects.all()
+        context = {
+            'bookdetails':bookdetails
+        }
+        return render(request, 'book.html', context)
+    
+class AuthorDetails(View):
+    def get (self, request):
+
+        authordetails = Author.objects.all()
+        context = {
+            'authordetails': authordetails
+        }
+        return render(request, 'authors.html', context)
+    
