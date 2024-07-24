aa8eb5935e1cceb0c2d54e3f1e34b5ab69a05ecc
Henrio <edenwannaji1980@gmail.com>
2024-07-24 13:55:30 +0100

    Fixed the static files rendering now

--- readit/readit/settings.py
+++ readit/readit/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATTICFILES_DIRS = (
- os.path.join(BASE_DIR, 'readit', ' static'),
-)
+STATTICFILES_DIRS = [
+ os.path.join(BASE_DIR, 'readit', 'books', ' static',)
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
